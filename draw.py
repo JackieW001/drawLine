2cb01d4c056553caaea1d16150cd01e1827cae4e
JackieW001 <jwoo@stuy.edu>
2018-02-08 21:29:54 -0500

    mod draw

--- draw.py
+++ draw.py
@@ -3,9 +3,16 @@ from display import *
 def draw_line( x0, y0, x1, y1, screen, color ):
     A = y1-y0
     B = -(x1-x0)
-    x = x0
-    y = y0
     slope = (A*1.0)/-B
+    
+    if (x0 < x1):
+        x = x0
+        y = y0
+    else:
+        x = x1
+        y = y1
+
+        
     #Octant I
     if( slope <= 1 and slope >= 0):
         d = (2*A) + B
@@ -50,7 +57,5 @@ def draw_line( x0, y0, x1, y1, screen, color ):
             y -= 1
             d -= (2*B)
 
-    else:
-        print "hi"
-        draw_line(x1, y1, x0, y1, screen, color)
+
 
