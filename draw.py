14a5bee0e514a27c617785c2c5c7f2b9e7d48796
JackieW001 <jwoo@stuy.edu>
2018-02-11 15:50:04 -0500

    done

--- draw.py
+++ draw.py
@@ -1,9 +1,6 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-    A = y1-y0
-    B = -(x1-x0)
-    slope = (A*1.0)/-B
     
     if (x0 < x1):
         x = x0
@@ -11,7 +8,24 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     else:
         x = x1
         y = y1
+        x1 = x0
+        y1 = y0
 
+    A = y1-y
+    B = -(x1-x)
+
+    # Slope = 0
+    if (B == 0):
+        if (y1 < y):
+            temp = y
+            y = y1
+            y1 = temp
+        while (y<=y1):
+            plot(screen, color, x, y)
+            y += 1
+        return
+    else:
+        slope = (A*1.0)/-B
         
     #Octant I
     if( slope <= 1 and slope >= 0):
--- main.py
+++ main.py
@@ -2,13 +2,43 @@ from display import *
 from draw import *
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
+color = [ 255, 255, 0 ]
 
 #draw_line(0,0,300,300, screen, color)
 #draw_line(0,0,300,500, screen, [255,0,0])
 #draw_line(0,400, 300,300, screen,[255,255,0])
 #draw_line(0, 400, 200, 100, screen, [0,255,255])
-draw_line(400, 400, 200, 200, screen, [255,255,255])
+#draw_line(250, 250, 250, 300, screen, [255,255,255])
+
+#axis
+draw_line(0, 250, 500, 250, screen, [51, 153, 102])
+draw_line(250, 0, 250, 500, screen, [51, 153, 102])
+draw_line(0, 0, 500, 500, screen, [51, 153, 102])
+draw_line(0, 500, 500, 0, screen, [51, 153, 102])
+
+def draw_rect(x, y, l, w, screen, color):
+    draw_line(x, y, x, y-l, screen, color) #left
+    draw_line(x, y, x+w, y, screen, color) #top
+    draw_line(x+w, y, x+w, y-l, screen, color) #right
+    draw_line(x+w, y-l, x, y-l, screen, color) #bottom
+
+x = 100
+y = 400
+l = 300
+r = 255
+g = 255
+b = 0
+
+while (x < 250):
+    draw_rect( x, y, l, l, screen, [r,g,b])
+    x += 10
+    y -= 10
+    l -= 20
+    
+    r -= 15
+    g -= 15
+    b += 15
+
 
 display(screen)
 save_extension(screen, 'img.png')
